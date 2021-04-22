26b99001ef81eef71627e9e46a5540389ea2481c
matkob <nologin90@gmail.com>
2021-04-22 21:37:56 +0200

    Add container rotation logic

--- load-balancer/containerized_app.py
+++ load-balancer/containerized_app.py
@@ -7,39 +7,36 @@ from docker import DockerClient
 from docker.errors import APIError
 from docker.models.containers import Container
 
+import utils
+
 
 class ContainerizedApp:
 
     @classmethod
     def create(cls, client: DockerClient):
-        environment = os.environ.get("ENVIRONMENT", "local")
-        hostname = f"mtd-app-{uuid.uuid4().hex}" if environment == "docker" else "localhost"
+        hostname = f"mtd-app-{uuid.uuid4().hex}"
         container = client.containers.run(image="spzc/python",
                                           name=hostname,
                                           volumes={f"mtd-webapp": {"bind": "/app", "mode": "ro"}},
                                           command=["python3", "app/serve.py"],
                                           detach=True,
                                           environment={"SESSION_MGR": "session-manager"},
-                                          network=os.environ.get("NETWORK_NAME", "default"),
+                                          network=os.environ.get("NETWORK_NAME", "mtd-private"),
                                           hostname=hostname)
         return ContainerizedApp(container, hostname)
 
     def __init__(self, container: Container, hostname: str):
         self.container: Container = container
-        self.hostname: str = hostname
+        self.hostname: str = hostname if os.environ.get("ENVIRONMENT", "local") == "docker" else "localhost"
         self.initialized: datetime.datetime = datetime.datetime.now()
-        self.logger = logging.getLogger(container.name)
-        self.logger.setLevel(logging.DEBUG)
-        h = logging.StreamHandler()
-        h.setLevel(logging.DEBUG)
-        self.logger.addHandler(h)
-        self.logger.info(f"created new app: {self.container.name}")
+        self.logger = utils.create_stdout_logger(logging.DEBUG, container.name)
+        self.logger.info(f"created new app")
 
     def remove(self):
-        self.logger.info(f"removing app: {self.container.name}")
+        self.logger.info(f"removing app")
         try:
             self.container.stop()
             self.container.remove()
-        except APIError:
-            self.logger.warning("could not remove app")
+        except APIError as err:
+            self.logger.warning(f"could not remove app: {err.explanation}")
         return self
--- load-balancer/controller.py
+++ load-balancer/controller.py
@@ -2,55 +2,67 @@ import logging
 import os
 import random
 import signal
+import datetime
 from typing import List
 
+from apscheduler.job import Job
+from pytimeparse.timeparse import timeparse
 import docker
 from apscheduler.schedulers.background import BackgroundScheduler
 from docker import DockerClient
+
+import utils
 from containerized_app import ContainerizedApp
 
 
 class Controller:
     LIVE_APPS = int(os.environ.get("REPLICAS", 1))
+    TIME_TO_LIVE = datetime.timedelta(seconds=timeparse(os.environ.get("APP_TTL", "5s")))
+    DECOMMISSION_PERIOD = datetime.timedelta(seconds=timeparse(os.environ.get("APP_DECOMMISSION_PERIOD", "10s")))
 
     def __init__(self):
         self.docker: DockerClient = docker.from_env()
         self.apps: List[ContainerizedApp] = []
+        self.next_apps: List[ContainerizedApp] = []
+        self.logger = utils.create_stdout_logger(logging.DEBUG, "controller")
 
-        self.logger = logging.getLogger("controller")
-        self.logger.setLevel(logging.DEBUG)
-        h = logging.StreamHandler()
-        h.setLevel(logging.DEBUG)
-        self.logger.addHandler(h)
-
-        self.initialize()
+        self.create_network()
+        self.apps = self.initialize_apps()
+        self.next_apps = self.initialize_apps()
 
         self.scheduler: BackgroundScheduler = BackgroundScheduler()
-        self.scheduler.add_job(func=lambda: self.rotate_apps(),
-                               trigger="interval",
-                               seconds=10,
-                               max_instances=1,
-                               replace_existing=False)
+        self.rotation_job: Job = self.scheduler.add_job(func=lambda: self.rotate_apps(),
+                                                        trigger="interval",
+                                                        seconds=self.TIME_TO_LIVE.seconds,
+                                                        max_instances=1,
+                                                        replace_existing=False)
         self.scheduler.start()
         signal.signal(signal.SIGTERM, lambda signum, frame: self.shutdown())
         signal.signal(signal.SIGINT, lambda signum, frame: self.shutdown())
 
-    def initialize(self):
-        for i in range(self.LIVE_APPS):
-            app = ContainerizedApp.create(self.docker)
-            self.apps.append(app)
+    def create_network(self):
+        network_name = os.environ.get("NETWORK_NAME", "mtd-private")
+        if len(self.docker.networks.list(names=[network_name])) == 0:
+            self.docker.networks.create(name=network_name, driver="bridge")
+
+    def initialize_apps(self):
+        return [ContainerizedApp.create(self.docker) for _ in range(self.LIVE_APPS)]
 
     def rotate_apps(self):
         self.logger.info("rotating apps")
-
-    def remove_apps(self):
-        [app.remove() for app in self.apps]
-        self.apps = []
+        old_apps = self.apps
+        self.scheduler.add_job(func=lambda: [app.remove() for app in old_apps],
+                               trigger="date",
+                               run_date=datetime.datetime.now() + self.DECOMMISSION_PERIOD)
+        self.apps = self.next_apps
+        self.next_apps = self.initialize_apps()  # race condition when shutting down during initialization
 
     def shutdown(self):
-        [app.remove() for app in self.apps]  # needs open client
-        self.docker.close()
+        self.rotation_job.remove()
+        [c.remove() for c in self.apps + self.next_apps]  # needs open client
+        [job.func() for job in self.scheduler.get_jobs()]
         self.scheduler.shutdown(wait=False)
+        self.docker.close()
         exit(0)
 
     def random_app(self) -> ContainerizedApp:
